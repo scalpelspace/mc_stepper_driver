a788d7819f89cdce98435a913582da7d31dc0dec
danielljeon <48095779+danielljeon@users.noreply.github.com>
2026-01-27 03:03:00 -0500

    Add high level mc_stepper_driver.h

---
+++ mc_stepper_driver.h
@@ -0,0 +1,15 @@
+/*******************************************************************************
+ * @file mc_stepper_driver.h
+ * @brief MC Stepper driver for CAN based communication.
+ *******************************************************************************
+ */
+
+#ifndef MC_STEPPER__DRIVER_H
+#define MC_STEPPER__DRIVER_H
+
+/** Includes. *****************************************************************/
+
+#include "can_driver/can_driver.h"
+#include "mc_stepper_can_dbc.h"
+
+#endif
