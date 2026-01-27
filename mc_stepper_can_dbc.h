e7d6ac57fb190e344e5df82ebd9473d94dbb8e6a
danielljeon <48095779+danielljeon@users.noreply.github.com>
2026-01-26 22:29:29 -0500

    Update DBC and auto-generate c representation

---
+++ mc_stepper_can_dbc.h
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * @file mc_stepper_can_dbc.h
+ * @brief Auto-generated CAN message definitions from DBC file.
+ *******************************************************************************
+ */
+
+#ifndef MC_STEPPER_CAN_DBC_H
+#define MC_STEPPER_CAN_DBC_H
+
+/** Includes. *****************************************************************/
+
+#include "can_driver/can_driver.h"
+
+/** CPP guard open. ***********************************************************/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** Public variables. *********************************************************/
+
+extern const can_message_t dbc_messages[];
+extern const int dbc_message_count;
+
+/** CPP guard close. **********************************************************/
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // MC_STEPPER_CAN_DBC_H
